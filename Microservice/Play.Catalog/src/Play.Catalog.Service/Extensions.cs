9137587d65c15d377fc04f08b3bedb24a6986cda
NghiaVNM <duongphanhieunghia2003@gmail.com>
2023-04-29 20:01:05 +0700

    Create Microservice

---
+++ Microservice/Play.Catalog/src/Play.Catalog.Service/Extensions.cs
@@ -0,0 +1,10 @@
+using Play.Catalog.Service.Dtos;
+using Play.Catalog.Service.Entities;
+
+namespace Play.Catalog.Service {
+    public static class Extension{
+        public static ItemDto AsDto(this Item item) {
+            
+        }
+    }
+}
